40b7f6d5e7899ae260706e9128c7f613ad304471
Andrey <andreykaere@gmail.com>
2025-12-28 18:13:18 +0300

    Change frontend to handle a single file

--- frontend/script.js
+++ frontend/script.js
@@ -1,58 +1,57 @@
-$(document).ready(function() {
+$(document).ready(function () {
   const $uploadContainer = $('#upload-container');
   const $fileInput = $('#file-input');
   const $uploadBtn = $('#upload-btn');
 
   // Button click triggers file input
-  $uploadBtn.on('click', function() {
+  $uploadBtn.on('click', function () {
     $fileInput.click();
   });
 
   // Drag over effect
-  $uploadContainer.on('dragover', function(e) {
+  $uploadContainer.on('dragover', function (e) {
     e.preventDefault();
     e.stopPropagation();
     $uploadContainer.addClass('dragover');
   });
 
-  $uploadContainer.on('dragleave', function(e) {
+  $uploadContainer.on('dragleave', function (e) {
     e.preventDefault();
     e.stopPropagation();
     $uploadContainer.removeClass('dragover');
   });
 
   // Handle file drop
-  $uploadContainer.on('drop', function(e) {
+  $uploadContainer.on('drop', function (e) {
     e.preventDefault();
     e.stopPropagation();
     $uploadContainer.removeClass('dragover');
-    const files = e.originalEvent.dataTransfer.files;
-    handleFiles(files);
+
+    const file = e.originalEvent.dataTransfer.files[0];
+    if (file) {
+      uploadFile(file);
+    }
   });
 
   // Handle file input selection
-  $fileInput.on('change', function() {
-    const files = this.files;
-    handleFiles(files);
+  $fileInput.on('change', function () {
+    const file = this.files[0];
+    if (file) {
+      uploadFile(file);
+    }
   });
 
-  function handleFiles(files) {
-    if (!files || files.length === 0) return;
-    
+  function uploadFile(file) {
     const formData = new FormData();
-    
-    for (let i = 0; i < files.length; i++) {
-        formData.append(`files`, files[i]);
-    }
+    formData.append("file", file); 
 
     fetch('/api/process', {
       method: 'POST',
       credentials: "include",
       body: formData,
     })
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.error('Error:', error));
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.error('Error:', error));
   }
 });
-
